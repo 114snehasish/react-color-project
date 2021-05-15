8f1b04b1d91a2db15acce90791c131ca08192629
114snehasish <114snehasish@gmail.com>
2021-05-15 19:21:48 +0530

    finish styling miniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -7,12 +7,17 @@ const styles = {
     border: '1px solid black',
     position: 'relative',
     overflow: 'hidden',
+    padding: '.5rem',
     '&:hover': {
       cursor: 'pointer',
     },
   },
   colors: {
-    backgroundColor: 'grey',
+    backgroundColor: '#dae1e4',
+    height: '150px',
+    width: '100%',
+    borderRadius: '5px',
+    overflow: 'hidden',
   },
   title: {
     display: 'flex',
@@ -28,13 +33,27 @@ const styles = {
     margin: '0.5rem',
     fontSize: '1.5rem',
   },
+  miniColor: {
+    height: '25%',
+    width: '20%',
+    display: 'inline-block',
+    margin: '0 auto',
+    position: 'relative',
+    marginBottom: '-4px',
+  },
 };
 function MiniPalette(props) {
-  const { classes, paletteName, emoji } = props;
-  console.log(classes);
+  const { classes, paletteName, emoji, colors } = props;
+  const miniPalette = colors.map((col) => (
+    <div
+      className={classes.miniColor}
+      style={{ backgroundColor: col.color }}
+      key={col.name}
+    ></div>
+  ));
   return (
     <div className={classes.root}>
-      <div className={classes.colors}></div>
+      <div className={classes.colors}>{miniPalette}</div>
       <h5 className={classes.title}>
         {paletteName}
         <span className={classes.emoji}>{emoji}</span>
