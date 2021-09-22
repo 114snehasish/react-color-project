ab37c5f459a453d9c2cf6418f49e1cd9761f4929
Snehasish Chakraborty <114snehasish@gmail.com>
2021-09-22 19:21:36 +0530

    start to style PaletteFormNav.js

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -9,6 +9,37 @@ import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
 import React, { useEffect } from "react";
+import { withStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const drawerWidth = 400;
+
+const useStyles = makeStyles((theme) => ({
+  navRoot: {
+    display: "flex",
+    height: "64px",
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  navBtns: {},
+}));
 
 const PaletteFormNav = ({
   classes,
@@ -18,6 +49,7 @@ const PaletteFormNav = ({
   savePalette,
 }) => {
   const [newPaletteName, setNewPaletteName] = React.useState("");
+  classes = { ...classes, ...useStyles() };
   useEffect(() => {
     ValidatorForm.addValidationRule("isPaletteNameUnique", (value) => {
       return palettes.every(
@@ -32,7 +64,7 @@ const PaletteFormNav = ({
   };
 
   return (
-    <>
+    <div className={classes.navRoot}>
       <CssBaseline />
       <AppBar
         position="fixed"
@@ -52,8 +84,10 @@ const PaletteFormNav = ({
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-            Persistent drawer
+            Create a Palette
           </Typography>
+        </Toolbar>
+        <div className={classes.navBtns}>
           <Link to="/">
             <Button variant="contained" color="secondary">
               Go Back
@@ -74,9 +108,9 @@ const PaletteFormNav = ({
               Save Palette
             </Button>
           </ValidatorForm>
-        </Toolbar>
+        </div>
       </AppBar>
-    </>
+    </div>
   );
 };
 
