45246d4598971bce637b8276d5bd11f15c3673ac
Snehasish Chakraborty <114snehasish@gmail.com>
2021-10-17 18:06:26 +0530

    make palettelist responsive

--- src/Styles/PaletteListStyles.js
+++ src/Styles/PaletteListStyles.js
@@ -1,33 +1,47 @@
+import sizes from './Sizes';
+
 export const styles = {
-    root: {
-        backgroundColor: 'blue',
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'flex-start',
-        justifyContent: 'center',
+  root: {
+    backgroundColor: 'blue',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  container: {
+    width: '50%',
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    [sizes.down('xl')]: {
+      width: '80%',
     },
-    container: {
-        width: '50%',
-        display: 'flex',
-        alignItems: 'flex-start',
-        flexDirection: 'column',
-        flexWrap: 'wrap',
+    [sizes.down('xs')]: {
+      width: '70%',
     },
-    nav: {
-        display: 'flex',
-        width: '100%',
-        justifyContent: 'space-between',
-        color: 'white',
-        alignItems: 'center',
-        '& a': {
-            color: 'white',
-        }
+  },
+  nav: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+    color: 'white',
+    alignItems: 'center',
+    '& a': {
+      color: 'white',
     },
-    palette: {
-        boxSizing: 'border-box',
-        width: '100%',
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 30%)',
-        gridGap: '5%',
+  },
+  palette: {
+    boxSizing: 'border-box',
+    width: '100%',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 30%)',
+    gridGap: '1.5rem',
+    [sizes.down('md')]: {
+      gridTemplateColumns: 'repeat(2, 50%)',
     },
+    [sizes.down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)',
+    },
+  },
 };
